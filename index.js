0c6ddd8cf784afb7d4c67a5884dbd026e94bc0a9
French <frenchcyrilsambilad@gmail.com>
2026-03-08 15:55:41 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+}); 
